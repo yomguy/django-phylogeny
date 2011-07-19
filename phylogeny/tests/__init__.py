c6c0115a872c6b7bc958c22053665d208cd11146
Randall Morey <randall@randallmorey.com>
2011-07-18 23:23:05 -0400

    added tests for phylogeny importers

--- phylogeny/tests/__init__.py
+++ phylogeny/tests/__init__.py
@@ -1,1 +1,1 @@
-from phylogeny.tests.default import GeneralPhylogenyTestCase, PhyloExporterTestCase, PhyloExporterRegistryTestCase
+from phylogeny.tests.default import GeneralPhylogenyTestCase, PhyloExporterTestCase, PhyloExporterRegistryTestCase, PhyloImporterRegistryTestCase
